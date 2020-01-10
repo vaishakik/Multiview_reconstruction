03a4ea4839ecf7265b62d09d89fc667aa4a17950
Why?shock! <vaishak.i.kuppast@gmail.com>
2020-01-10 19:24:39 +0530

    Update download_redwood_objects.sh

--- scripts/download_redwood_objects.sh
+++ scripts/download_redwood_objects.sh
@@ -13,9 +13,9 @@ process_redwood_object()
 }
 
 mkdir ../dataset/redwood_objects
-process_redwood_object "00021" "chair" "1iMxjIZMFcoL3s9FzzqM0K-tM2ehO74D0"
-process_redwood_object "00577" "sofa" "1_WZK0AZTt7N3QBh9JnDypM1sPKnE0oIY"
+#process_redwood_object "00021" "chair" "1iMxjIZMFcoL3s9FzzqM0K-tM2ehO74D0"
+#process_redwood_object "00577" "sofa" "1_WZK0AZTt7N3QBh9JnDypM1sPKnE0oIY"
 process_redwood_object "01833" "car" "1HCRVzlZ0huAsTyQsjOvI2OsZ_oU3zpt9"
 process_redwood_object "05984" "motorcycle" "1jZLTNrOIP2sFgzF9sHb027iI2JhOOqpu"
-process_redwood_object "06127" "plant" "1nWilJfkAA7D3a8JEc_Tx9pLD4a25u8xG"
-process_redwood_object "06822" "truck" "1TqkWcdzQZG50ZV9nXdZYZmR_aLH-WYkr"
+#process_redwood_object "06127" "plant" "1nWilJfkAA7D3a8JEc_Tx9pLD4a25u8xG"
+#process_redwood_object "06822" "truck" "1TqkWcdzQZG50ZV9nXdZYZmR_aLH-WYkr"
